6a59f0b035bad3f720f35d24dedb723a078636ae
Andrew <aiglinski@icloud.com>
2019-08-31 17:21:27 -0500

    fully functional svg for landing page but design needs major work, no functionality either beyond landing graphic

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const logoText = document.querySelectorAll('#logo-text path');
+for (let i = 0; i < logoText.length; i++) {
+    console.log(`letter ${i} is ${logoText[i].getTotalLength()}`)
+}
+
+
 const logo = document.querySelectorAll('#logo path');
 for (let i = 0; i < logo.length; i++) {
-    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`)
+    console.log(`Element ${i} is ${logo[i].getTotalLength()}`)
 }
