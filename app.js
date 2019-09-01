f8b1965b46fc013fd36e9f4f0d3573bef2855ac8
Andrew <aiglinski@icloud.com>
2019-08-31 23:02:19 -0500

    saving some minor design tweaks, tomorrow need to add second section and make sure section one is set to a height of 100vh. Also make mobile menu expansion functional

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const logoText = document.querySelectorAll('#logo-text path');
-for (let i = 0; i < logoText.length; i++) {
-    console.log(`letter ${i} is ${logoText[i].getTotalLength()}`)
-}
+// const logoText = document.querySelectorAll('#logo-text path');
+// for (let i = 0; i < logoText.length; i++) {
+//     console.log(`letter ${i} is ${logoText[i].getTotalLength()}`)
+// }
 
 
-const logo = document.querySelectorAll('#logo path');
-for (let i = 0; i < logo.length; i++) {
-    console.log(`Element ${i} is ${logo[i].getTotalLength()}`)
-}
+// const logo = document.querySelectorAll('#logo path');
+// for (let i = 0; i < logo.length; i++) {
+//     console.log(`Element ${i} is ${logo[i].getTotalLength()}`)
+// }
