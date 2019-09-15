57d3f55f9f10a9fa281772ca524d4d2cb6f0eb1a
Andrew <aiglinski@icloud.com>
2019-09-15 13:21:58 -0500

    before starting switch to object method, saving incase I break everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var settings = {
+    var settingsRev = {
         "async": true,
         "crossDomain": true,
         "url": "https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/RQZCOKGctMfuf-MRVUdVnw/reviews",
@@ -8,33 +8,76 @@ $(document).ready(function () {
             "Content-Type": "application/json",
             "Authorization": "Bearer ng-wZ3Z6BAY_7Qj5LvRPzxE5Uw_64fwJFF0YWaIf1zHD6QU5zzraEO3EaJ7qYAKr0LCrwwD_dahPHqHYFZ5v1fcAsAdghBwHY_jRf_nzKJUct9SKW8jW_X7mD4F9XXYx",
 
-            "Accept": "*/*",
-            "Cache-Control": "no-cache",
-            "Postman-Token": "e9ee7a20-1f34-4638-afcf-ed5949468f1a,3e08b223-c327-4c0c-9320-47a06bb0dde0",
+        },
+        "processData": false,
+        "data": ""
+    }
+    var settingsGen = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/RQZCOKGctMfuf-MRVUdVnw",
+        "method": "GET",
+        "headers": {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer ng-wZ3Z6BAY_7Qj5LvRPzxE5Uw_64fwJFF0YWaIf1zHD6QU5zzraEO3EaJ7qYAKr0LCrwwD_dahPHqHYFZ5v1fcAsAdghBwHY_jRf_nzKJUct9SKW8jW_X7mD4F9XXYx",
 
-
-
-            "cache-control": "no-cache"
         },
         "processData": false,
         "data": ""
     }
 
-    $.ajax(settings).done(function (response) {
-        var nameOne = response.reviews[0].user.name;
+
+
+
+
+    const threeStars = "img/3.png";
+    const threeHalf = "img/3andhalf.png";
+    const fourStars = "img/4.png";
+    const fourHalf = "img/4andhalf.png";
+    const fiveStars = "img/5.png";
+
+
+    $.ajax(settingsRev).done(function (responseReviews) {
+
+        for (var i = 0; i < responseReviews.length; i++) {
+            console.log('success')
+        }
+
+        var nameOne = responseReviews.reviews[0].user.name;
         document.getElementById("textTitle1").innerHTML = `${nameOne}`
-        var textOne = response.reviews[0].text;
+        var textOne = responseReviews.reviews[0].text;
         document.getElementById("review1Text").innerHTML = `${textOne}`;
+        var ratingOne = responseReviews.reviews[0].rating;
+        document.getElementById("RatingOne").src =
+            console.log(ratingOne);
 
-        var nameTwo = response.reviews[1].user.name;
+        var nameTwo = responseReviews.reviews[1].user.name;
         document.getElementById("textTitle2").innerHTML = `${nameTwo}`
-        var textTwo = response.reviews[1].text;
+        var textTwo = responseReviews.reviews[1].text;
         document.getElementById("review2Text").innerHTML = `${textTwo}`;
+        var ratingTwo = responseReviews.reviews[1].rating;
+        console.log(ratingTwo);
 
-        var nameThree = response.reviews[2].user.name;
+        var nameThree = responseReviews.reviews[2].user.name;
         document.getElementById("textTitle3").innerHTML = `${nameThree}`
-        var textThree = response.reviews[2].text;
+        var textThree = responseReviews.reviews[2].text;
         document.getElementById("review3Text").innerHTML = `${textThree}`;
+        var ratingThree = responseReviews.reviews[2].rating;
+        console.log(ratingThree);
+
+        console.log(responseReviews)
+
+    });
+
+
+    $.ajax(settingsGen).done(function (responseGen) {
+        var x = responseGen;
+        console.log(x);
+
+        // var nameOne = responseGen.reviews[0].user.name;
+        // document.getElementById("textTitle1").innerHTML = `${nameOne}`
+        // var textOne = responseGen.reviews[0].text;
+        // document.getElementById("review1Text").innerHTML = `${textOne}`;
 
     });
 });
