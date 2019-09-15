ac6ce54c10e5518c69cf3f83ceb038ab80d089ef
Andrew <aiglinski@icloud.com>
2019-09-14 19:29:09 -0500

    innerHtml and yelp API functional, but still need to add review count and average rating to section 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/blue-star-cafe-milwaukee",
+        "url": "https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/RQZCOKGctMfuf-MRVUdVnw/reviews",
         "method": "GET",
         "headers": {
             "Content-Type": "application/json",
-            "Authorization": "Bearer LnulvU_jLskBmtPQdn0XDIhhnHjCR8GnMNb2n3MoRk_SK8LmPOQljO_oFm_rMVrLs9GRd84_uoQ38C6-AkUaKEF0is9TaEr-GcMxQwi8W4J9hXG6z4h8jRWpRehrXXYx",
-            "User-Agent": "PostmanRuntime/7.16.3",
+            "Authorization": "Bearer ng-wZ3Z6BAY_7Qj5LvRPzxE5Uw_64fwJFF0YWaIf1zHD6QU5zzraEO3EaJ7qYAKr0LCrwwD_dahPHqHYFZ5v1fcAsAdghBwHY_jRf_nzKJUct9SKW8jW_X7mD4F9XXYx",
+
             "Accept": "*/*",
             "Cache-Control": "no-cache",
             "Postman-Token": "e9ee7a20-1f34-4638-afcf-ed5949468f1a,3e08b223-c327-4c0c-9320-47a06bb0dde0",
-            "Host": "api.yelp.com",
-            "Accept-Encoding": "gzip, deflate",
-            "Connection": "keep-alive",
+
+
+
             "cache-control": "no-cache"
         },
         "processData": false,
@@ -21,6 +21,20 @@ $(document).ready(function () {
     }
 
     $.ajax(settings).done(function (response) {
-        console.log(response);
+        var nameOne = response.reviews[0].user.name;
+        document.getElementById("textTitle1").innerHTML = `${nameOne}`
+        var textOne = response.reviews[0].text;
+        document.getElementById("review1Text").innerHTML = `${textOne}`;
+
+        var nameTwo = response.reviews[1].user.name;
+        document.getElementById("textTitle2").innerHTML = `${nameTwo}`
+        var textTwo = response.reviews[1].text;
+        document.getElementById("review2Text").innerHTML = `${textTwo}`;
+
+        var nameThree = response.reviews[2].user.name;
+        document.getElementById("textTitle3").innerHTML = `${nameThree}`
+        var textThree = response.reviews[2].text;
+        document.getElementById("review3Text").innerHTML = `${textThree}`;
+
     });
 });
