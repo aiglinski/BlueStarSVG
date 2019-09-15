4733b5268fcd0ea9c41ed87624f88c48047d5628
Andrew <aiglinski@icloud.com>
2019-09-15 14:02:03 -0500

    yelp api fully functional with inserted html, rating references to images, rating count and average inserted into text

--- js/scripts.js
+++ js/scripts.js
@@ -27,57 +27,43 @@ $(document).ready(function () {
     }
 
 
-
-
-
-    const threeStars = "img/3.png";
-    const threeHalf = "img/3andhalf.png";
-    const fourStars = "img/4.png";
-    const fourHalf = "img/4andhalf.png";
-    const fiveStars = "img/5.png";
-
-
     $.ajax(settingsRev).done(function (responseReviews) {
 
-        for (var i = 0; i < responseReviews.length; i++) {
-            console.log('success')
+        var reviewOne = {
+            name: responseReviews.reviews[0].user.name,
+            text: responseReviews.reviews[0].text,
+            rating: responseReviews.reviews[0].rating,
         }
+        document.getElementById('textTitle1').innerHTML = reviewOne.name;
+        document.getElementById('review1Text').innerHTML = reviewOne.text;
+        document.getElementById('RatingOne').src = `/img/${reviewOne.rating}.png`;
 
-        var nameOne = responseReviews.reviews[0].user.name;
-        document.getElementById("textTitle1").innerHTML = `${nameOne}`
-        var textOne = responseReviews.reviews[0].text;
-        document.getElementById("review1Text").innerHTML = `${textOne}`;
-        var ratingOne = responseReviews.reviews[0].rating;
-        document.getElementById("RatingOne").src =
-            console.log(ratingOne);
+        var reviewTwo = {
+            name: responseReviews.reviews[1].user.name,
+            text: responseReviews.reviews[1].text,
+            rating: responseReviews.reviews[1].rating,
+        }
+        document.getElementById('textTitle2').innerHTML = reviewTwo.name;
+        document.getElementById('review2Text').innerHTML = reviewTwo.text;
+        document.getElementById('RatingTwo').src = `/img/${reviewTwo.rating}.png`;
 
-        var nameTwo = responseReviews.reviews[1].user.name;
-        document.getElementById("textTitle2").innerHTML = `${nameTwo}`
-        var textTwo = responseReviews.reviews[1].text;
-        document.getElementById("review2Text").innerHTML = `${textTwo}`;
-        var ratingTwo = responseReviews.reviews[1].rating;
-        console.log(ratingTwo);
-
-        var nameThree = responseReviews.reviews[2].user.name;
-        document.getElementById("textTitle3").innerHTML = `${nameThree}`
-        var textThree = responseReviews.reviews[2].text;
-        document.getElementById("review3Text").innerHTML = `${textThree}`;
-        var ratingThree = responseReviews.reviews[2].rating;
-        console.log(ratingThree);
-
-        console.log(responseReviews)
+        var reviewThree = {
+            name: responseReviews.reviews[2].user.name,
+            text: responseReviews.reviews[2].text,
+            rating: responseReviews.reviews[2].rating,
+        }
+        document.getElementById('textTitle3').innerHTML = reviewThree.name;
+        document.getElementById('review3Text').innerHTML = reviewThree.text;
+        document.getElementById('RatingThree').src = `/img/${reviewThree.rating}.png`;
 
     });
 
 
     $.ajax(settingsGen).done(function (responseGen) {
-        var x = responseGen;
-        console.log(x);
+        var count = responseGen.review_count;
+        var ratingAve = responseGen.rating;
 
-        // var nameOne = responseGen.reviews[0].user.name;
-        // document.getElementById("textTitle1").innerHTML = `${nameOne}`
-        // var textOne = responseGen.reviews[0].text;
-        // document.getElementById("review1Text").innerHTML = `${textOne}`;
+        document.getElementById('reviewCount').innerHTML = `Currently there are ${count} with an average of ${ratingAve} stars`
 
     });
 });
